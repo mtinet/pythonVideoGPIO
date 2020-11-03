22796688c48258ea88ff56a5f54670098ecf1351
Jason Juhyun Kim <mtinet@hanmail.net>
2020-11-03 17:33:02 +0900

    Update complete.py

--- complete.py
+++ complete.py
@@ -12,14 +12,11 @@ VIDEO_PATH = Path("black.mp4")
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(18, GPIO.IN)
 
-player = OMXPlayer(VIDEO_PATH)
-sleep(3)
-player.quit()
-
 try:
     while True:
         if GPIO.input(18) == 0:
             player = OMXPlayer(VIDEO_PATH)
+            # 이 부분으로 재생 시간 조정, 초 단위
             sleep(3)
             player.quit()
         else :
